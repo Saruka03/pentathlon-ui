2af76554cb88144547bf6d1deeff0c56f8acc1c0
Saruka Umainesan <umainesansaruka@gmail.com>
2026-03-07 16:53:19 +0530

    Updated

--- src/main.tsx
+++ src/main.tsx
@@ -4,20 +4,7 @@ import { BrowserRouter } from "react-router-dom"
 import App from "./App"
 import "./index.css"
 
-// 🔹 Vite-aware basename (dev + production safe)
 const basename = import.meta.env.BASE_URL
-
-// 🔹 HANDLE GITHUB PAGES REDIRECT
-const params = new URLSearchParams(window.location.search)
-const redirect = params.get("redirect")
-
-if (redirect) {
-  window.history.replaceState(
-    null,
-    "",
-    redirect.replace(basename, "/")
-  )
-}
 
 ReactDOM.createRoot(
   document.getElementById("root")!
